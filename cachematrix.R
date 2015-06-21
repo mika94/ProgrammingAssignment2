d02ac290d534adbf82f39464ec389bfecf5e91c2
Caroline <seilerc900@gmail.com>
2015-06-20 21:43:00 -0300

    Completed Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## The following functions are used to calculate and then cache the inverse of a matrix so that the next time the inverse is used, it is taken out of the cache rather than computed repeatedly.
 
 ## Write a short comment describing this function
 
